bf843427670c5fe4c1f8ec683e597008fbd3a21f
Aarya G <ghongadeaarya2@gmail.com>
2026-06-15 18:49:13 +0530

    Add files via upload

---
+++ Bill_split_calculator.py
@@ -0,0 +1,6 @@
+#BILL SPLIT CALCULATOR
+amount=int(input("Enter the total amout:"))
+people=int(input("enter total amount of people"))
+money_split=(amount/people)
+print("The amount paid by each person is", money_split)
+print(type(money_split))
